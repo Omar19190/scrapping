950e54d9ae71026b2ca9f71b7c53735ec8ec0053
Omar19190 <120134971+Omar19190@users.noreply.github.com>
2022-12-17 23:36:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ src = page.content
 #print(page.content)
 soup = bs4.BeautifulSoup(src,"html.parser")
 
-wallet_titles = []
-wallet_prices = []
+short_titles = []
+short_prices = []
 
-wallet_title = soup.find_all("h3",{"class":"product-card__title"})
+short_title = soup.find_all("h3",{"class":"product-card__title"})
 
-wallet_price = soup.find_all("div",{"class":"product-card__price--new d-inline-flex"})
+short_price = soup.find_all("div",{"class":"product-card__price--new d-inline-flex"})
 
 
-for i in range(len(wallet_title)):
-    wallet_titles.append(wallet_title[i].text)
-    wallet_prices.append(wallet_price[i].text)
-print(wallet_titles,wallet_prices)
+for i in range(len(short_title)):
+    short_titles.append(short_title[i].text)
+    short_prices.append(short_price[i].text)
+print(short_titles,short_prices)
